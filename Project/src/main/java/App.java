956d09ac0b120329b100a7c95b9fabc3ce6bfcf0
Leo <lchristmann2@gmail.com>
2023-11-24 23:47:21 +0100

    .

--- Project/src/main/java/App.java
+++ Project/src/main/java/App.java
@@ -1,12 +1,21 @@
 import java.io.IOException;
-// import org.jsoup.*;
-// import org.jsoup.nodes.*;
+import org.jsoup.*;
+import org.jsoup.nodes.*;
 
 public class App {
     public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
 
-        // String url = "https://www.google.com";
-        // Document pageToScrape = Jsoup.connect(url).get();
+        String[] data = FileReader.scanData("../data.txt");
+        String url = data[0];
+        String user = data[1];
+        String pw = data[2];
+
+        System.out.println(url);
+        System.out.println(user);
+        System.out.println(pw);
+
+        Document pageToScrape = Jsoup.connect(url).get();
+        System.out.println(pageToScrape);
     }
 }
---
+++ Project/src/main/java/FileReader.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileReader {
+
+  public static String[] scanData(String filepath) throws FileNotFoundException {
+
+    String[] data = new String[3];
+
+    try {
+      File file = new File(filepath);
+      Scanner scan = new Scanner(file);
+
+      int i = 0;
+      while (scan.hasNextLine()) {
+        String line = scan.nextLine();
+        data[i] = line;
+        i++;
+      }
+      scan.close();
+    } catch (Exception e) {
+      System.out.println("Hoppalla...");
+      e.printStackTrace();
+    }
+    return data;
+  }
+}
