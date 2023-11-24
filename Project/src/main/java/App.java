3e1be517632cca63ff42feef834c452ea2bfed29
Leo <lchristmann2@gmail.com>
2023-11-24 23:18:57 +0100

    .

---
+++ Project/src/main/java/App.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello world!");
+
+        // String url = "https://www.google.com";
+        // Document pageToScrape = Jsoup.connect(url).get();
+    }
+}
