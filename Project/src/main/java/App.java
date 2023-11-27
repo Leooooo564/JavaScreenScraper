853db11e6d2004208485718765ff8b6011f40517
Leo <lchristmann2@gmail.com>
2023-11-27 21:51:21 +0100

    change to selenium

--- Project/src/main/java/App.java
+++ Project/src/main/java/App.java
@@ -1,26 +1,41 @@
 import java.io.IOException;
-import org.jsoup.*;
-import org.jsoup.nodes.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello world!");
-
-        String[] data = FileReader.scanData("/home/pi/data.txt");
+        String[] data = FileReader.scanData("../data.txt");
         String url = data[0];
         String user = data[1];
         String pw = data[2];
+        
 
-        System.out.println(url);
-        System.out.println(user);
-        System.out.println(pw);
+        System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get(url);
+        System.out.println(" \n \n\\ ");
 
-        try{
-            Document pageToScrape = Jsoup.connect(url).get();
-            System.out.println(pageToScrape.outerHtml());
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
+
+        WebElement UsernameInput = driver.findElement(By.id("username2"));
+        WebElement PasswordInput = driver.findElement(By.id("inputPassword"));
+
+        UsernameInput.sendKeys(user);
+        PasswordInput.sendKeys(pw);
+
+        WebElement LoginButton = driver.findElement(By.id("tlogin"));
+        LoginButton.click();
+
+        String currentUrl = driver.getCurrentUrl();
+        new WebDriverWait(driver, 1).until(ExpectedConditions.urlChanges(url));
+
+        WebElement tempWidgetConatiner = driver.findElement(By.cssSelector("div"));
+        WebElement VPWidget = tempWidgetConatiner.findElement(By.cssSelector("a[href]"));
+        VPWidget.click();
+        // driver.quit();
+        
     }
 }
