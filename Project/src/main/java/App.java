9d0c1ae3963e5d69f1ff350328a711dfa7016f26
Leo <lchristmann2@gmail.com>
2023-11-29 20:19:17 +0100

    .

--- Project/src/main/java/App.java
+++ Project/src/main/java/App.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.time.Duration;
 
-// import java.time.Duration;
+import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
